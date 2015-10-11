cf8ec807773cd2673d56541dd91dfeaaa11dc2a0
Judson Silva <judsonbarroso@gmail.com>
2015-10-10 22:03:59 -0300

    Add feed view and test camera

---
+++ client/views/feed/feed.js
@@ -0,0 +1,15 @@
+Template.feed.events({
+	'click #camera-caller': function(event, template){
+		var self = this;
+		MeteorCamera.getPicture({
+			width: 600,
+			height: 600,
+			quality: 80
+		}, function( error, data ){
+			if( !error ){
+				console.log(data);
+				$('#image').attr('src',data);
+			}
+		});
+	}
+});
